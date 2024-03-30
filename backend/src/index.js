d401024a952f908a23ff347cf25e64bcfc0b9be1
syket-das <saiketsd23@gmail.com>
2024-03-30 11:49:42 +0530

    wd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
